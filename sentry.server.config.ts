27e2fd1801ea1199694eb210b700942f5a9b99be
thekevinkun <developer.kevinkun@gmail.com>
2026-03-29 21:46:40 +0800

    chore: configure Sentry in Next.js
    - enable client and server error tracking
    - route browser requests through API to avoid ad blockers
    - initialize Sentry SDK

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b073ab20fe3feae48f72e2318459ffe4@o4511127754637312.ingest.us.sentry.io/4511127756275712",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
